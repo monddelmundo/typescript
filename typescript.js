df9daa0e928c521625c7289b8af28be3f337666a
Edmond Del Mundo <edmond.delmundo@gmail.com>
2020-10-21 21:04:44 +0800

    Added more types

--- typescript.js
+++ typescript.js
@@ -1,17 +1,28 @@
-//boolean
+// boolean
 var isCool = true;
-//number
+// number
 var age = 56;
-//string
+// string
 var eyeColor = "brown";
 var favoritQuote = "I'm not old, I'm only " + age;
-//Arrays
+// Arrays
 var pets = ["cat", "dog", "pig"];
 var pets2 = ["lion", "dragon", "lizard"];
-//Object
+// Object
 var wizard = {
     a: "John"
 };
-//null and undefined
+// null and undefined
 var meh = undefined;
 var mah = null;
+// tuple
+var basket;
+basket = ["basketball", 5];
+// enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size[2];
--- typescript.ts
+++ typescript.ts
@@ -1,22 +1,34 @@
-//boolean
+// boolean
 let isCool: boolean = true;
 
-//number
+// number
 let age: number = 56;
 
-//string
+// string
 let eyeColor: string = "brown";
 let favoritQuote: string = `I'm not old, I'm only ${age}`;
 
-//Arrays
+// Arrays
 let pets: string[] = ["cat", "dog", "pig"];
 let pets2: Array<string> = ["lion", "dragon", "lizard"];
 
-//Object
+// Object
 let wizard: object = {
   a: "John",
 };
 
-//null and undefined
+// null and undefined
 let meh: undefined = undefined;
 let mah: null = null;
+
+// tuple
+let basket: [string, number];
+basket = ["basketball", 5];
+
+// enum
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
+let sizeName: string = Size[2];
